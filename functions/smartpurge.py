f4da5fe4d2721e3729102b400a7981770b9b476f
Gleb Smirnov <glebsmirnov0708@gmail.com>
2021-11-19 12:45:57 +0300

    Fix smartpurge and move it to functions

---
+++ functions/smartpurge.py
@@ -0,0 +1,10 @@
+# Filter and delete messages up to given
+
+@silent
+async def smartpurge(func):
+    orig = await ctx.msg.get_reply_message()
+    async for msg in client.iter_messages(ctx.msg.chat_id):
+        if func(msg):
+            await msg.delete()
+        if msg == orig:
+            break
